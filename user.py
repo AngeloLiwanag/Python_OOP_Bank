73154f9fd417e21fa2b61187977dd9fbf44ca174
Angelo Liwanag <AngeloLiwanag75@gmail.com>
2019-06-04 14:54:41 -0700

    second commit

--- user.py
+++ user.py
@@ -6,37 +6,24 @@ class User:
 
     def make_deposit(self,amount):
         self.account_balance += amount
+        return self
     
     def make_widthdrawl(self,amount):
         if self.account_balance < 0:
             print("There is no money to widthdraw")
         else:
             self.account_balance -= amount
+            return self
     
     def display_user_balance(self):
         amount = self.account_balance
         print(self.name,"you have a total balance of",amount)
+        return self
 
 angelo = User("Angelo","angelo@python.com")
 kathy = User("Kathy","kathy@python.com")
 kendrick = User("Kendrick", "kendrick@python.com")
 
-angelo.make_deposit(100)
-angelo.make_deposit(100)
-angelo.make_deposit(50)
-angelo.display_user_balance()
-angelo.make_widthdrawl(200)
-angelo.display_user_balance()
-
-kathy.make_deposit(90)
-kathy.make_deposit(220)
-kathy.display_user_balance()
-kathy.make_widthdrawl(90)
-kathy.make_widthdrawl(5)
-kathy.display_user_balance()
-
-kendrick.make_deposit(10)
-kendrick.make_widthdrawl(100)
-kendrick.make_widthdrawl(100)
-kendrick.make_widthdrawl(100)
-kendrick.display_user_balance()
+angelo.make_deposit(100).make_deposit(100).make_deposit(50).display_user_balance().make_widthdrawl(200).display_user_balance()
+kathy.make_deposit(90).make_deposit(220).display_user_balance().make_widthdrawl(90).make_widthdrawl(5).display_user_balance()
+kendrick.make_deposit(10).make_widthdrawl(100).make_widthdrawl(100).make_widthdrawl(100).display_user_balance()
